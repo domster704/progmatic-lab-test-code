ff46bf420d364b2a424ee6d4577a5508e3cf7b54
Grigoriy <61056244+domster704@users.noreply.github.com>
2024-02-01 14:19:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ digits = list(range(9, -1, -1))
 operators = ['+', '-']
 res = 200
 
+resultList = set([])
 for op_combination in itertools.product(operators, repeat=len(digits) - 1):
     expression = [str(digits[i]) + op for i, op in enumerate(op_combination)]
     expression.append(str(digits[-1]))
@@ -16,8 +17,13 @@ for op_combination in itertools.product(operators, repeat=len(digits) - 1):
             localExpression[pos] = localExpression[pos][0]
 
         resultExpressionInStrFormat = ''.join(localExpression)
-        if eval(resultExpressionInStrFormat) == res:
+        if eval(resultExpressionInStrFormat) == res and resultExpressionInStrFormat not in resultList:
+            resultList.add(resultExpressionInStrFormat)
             print(resultExpressionInStrFormat)
-            exit(0)
 
-# для res=200 результат - 98+76-5+43-2-10
+# для res=200 результат: 
+# 98+76-5+43-2-10
+# 98-7+65+43+2-1+0
+# 98-7+65+43+2-1-0
+# 9-8+7-6-5-4-3+210
+# 9-8-7-6-5+4+3+210
